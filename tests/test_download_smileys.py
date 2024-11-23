9c29bd0379d57ab5e50e9ff94397f81c2fdf87fd
reborn12345 <58338231+reborn12345@users.noreply.github.com>
2024-11-23 21:28:33 +0100

    Ruff format test_download_smileys.py

--- tests/test_download_smileys.py
+++ tests/test_download_smileys.py
@@ -1,6 +1,7 @@
 from download_smileys import SmileyDownloader
 from pathlib import Path
-import shutil # for cleanup
+import shutil  # for cleanup
+
 
 def test_smiley_download():
     url = "https://jvflux.fr/JVFlux:Liste_des_smileys"
@@ -8,12 +9,12 @@ def test_smiley_download():
     downloader = SmileyDownloader(url, download_dir=test_dir)
 
     try:
-      downloader.download_smileys()
-      # Assertions: Check if files exist, are the correct size, etc.
-      assert test_dir.exists()
-      assert any(test_dir.iterdir())  # Check if the directory is not empty
+        downloader.download_smileys()
+        # Assertions: Check if files exist, are the correct size, etc.
+        assert test_dir.exists()
+        assert any(test_dir.iterdir())  # Check if the directory is not empty
 
     finally:
-      # Cleanup: Remove the test directory after the test
-      if test_dir.exists():
-        shutil.rmtree(test_dir)
+        # Cleanup: Remove the test directory after the test
+        if test_dir.exists():
+            shutil.rmtree(test_dir)
