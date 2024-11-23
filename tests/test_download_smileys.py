c28b04b239d464b103a763ce62d8ed37240ef18d
Reborn <38reborn@gmail.com>
2024-11-23 21:35:21 +0100

    Ruff formating/linter

--- tests/test_download_smileys.py
+++ tests/test_download_smileys.py
@@ -1,20 +1,21 @@
-from download_smileys import SmileyDownloader
-from pathlib import Path
 import shutil  # for cleanup
+from pathlib import Path
+
+from download_smileys import SmileyDownloader
 
 
 def test_smiley_download():
-    url = "https://jvflux.fr/JVFlux:Liste_des_smileys"
-    test_dir = Path("test_smileys")  # Test directory
-    downloader = SmileyDownloader(url, download_dir=test_dir)
+	url = 'https://jvflux.fr/JVFlux:Liste_des_smileys'
+	test_dir = Path('test_smileys')  # Test directory
+	downloader = SmileyDownloader(url, download_dir=test_dir)
 
-    try:
-        downloader.download_smileys()
-        # Assertions: Check if files exist, are the correct size, etc.
-        assert test_dir.exists()
-        assert any(test_dir.iterdir())  # Check if the directory is not empty
+	try:
+		downloader.download_smileys()
+		# Assertions: Check if files exist, are the correct size, etc.
+		assert test_dir.exists()
+		assert any(test_dir.iterdir())  # Check if the directory is not empty
 
-    finally:
-        # Cleanup: Remove the test directory after the test
-        if test_dir.exists():
-            shutil.rmtree(test_dir)
+	finally:
+		# Cleanup: Remove the test directory after the test
+		if test_dir.exists():
+			shutil.rmtree(test_dir)
