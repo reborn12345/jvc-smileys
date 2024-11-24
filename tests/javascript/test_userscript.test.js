a6fcd833704006d66bf6f86d57e2f2fea8b399d7
Reborn <38reborn@gmail.com>
2024-11-24 16:28:37 +0100

    correct require of js tests

--- tests/javascript/test_userscript.test.js
+++ tests/javascript/test_userscript.test.js
@@ -1,13 +1,20 @@
 // JVC_Smiley_animés.test.js
 
-const { restaurerSmileys } = require('../../userscripts/jvc-smileys.user.js'); // Importez la fonction
+const { restaurerSmileys } = require('../../userscripts/jvc-smileys.user.js');
 
-// Cette fonction sera appelée avant chaque test pour simuler un DOM
 beforeEach(() => {
   // Simuler un DOM de base avant chaque test
   document.body.innerHTML = `
     <img src="https://image.jeuxvideo.com/smileys_img/39.gif" alt=":rire:" data-code=":rire:" title=":rire:" width="16" height="16">
   `;
+
+  // Mock de la fonction fetch
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({ /* données simulées si nécessaire */ })
+    })
+  );
 });
 
 describe('restaurerSmileys', () => {
@@ -16,6 +23,9 @@ describe('restaurerSmileys', () => {
 
     // Vérifier si l'image a été correctement modifiée
     const img = document.querySelector('img[data-code=":rire:"]');
-    expect(img.src).toContain('https://reborn12345.github.io/jvc-smileys/smileys/39.gif'); // Vérifier l'URL du smiley animé
+    expect(img.src).toContain('https://reborn12345.github.io/jvc-smileys/smileys/39.gif');
+    
+    // Vérifier si fetch a été appelé correctement
+    expect(fetch).toHaveBeenCalledWith('https://reborn12345.github.io/jvc-smileys/smileys/39.gif');
   });
 });
